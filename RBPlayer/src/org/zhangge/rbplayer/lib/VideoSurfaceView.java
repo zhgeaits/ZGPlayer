e56d5bdc8abaa1101ede99c8b081a019e3cb8235
zhangge <zhgeaits@gmail.com>
2014-10-19 11:51:53 +0800

    init lib

---
+++ RBPlayer/src/org/zhangge/rbplayer/lib/VideoSurfaceView.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.zhangge.rbplayer.lib;
+
+import java.io.IOException;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+import android.view.Surface;
+
+public class VideoSurfaceView extends GLSurfaceView {
+
+    VideoRender mRenderer;
+    private MediaPlayer mMediaPlayer = null;
+
+    public VideoSurfaceView(Context context, MediaPlayer mp) {
+        super(context);
+
+        setEGLContextClientVersion(2);
+        mMediaPlayer = mp;
+        mRenderer = new VideoRender(context);
+        setRenderer(mRenderer);
+        queueEvent(new Runnable(){
+            public void run() {
+                mRenderer.setMediaPlayer(mMediaPlayer);
+            }});
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    /**
+     * A GLSurfaceView implementation that wraps TextureRender.  Used to render frames from a
+     * video decoder to a View.
+     */
+    private static class VideoRender
+            implements GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener {
+        private static String TAG = "VideoRender";
+
+        private TextureRender mTextureRender;
+        private SurfaceTexture mSurfaceTexture;
+        private boolean updateSurface = false;
+
+        private MediaPlayer mMediaPlayer;
+
+        public VideoRender(Context context) {
+            mTextureRender = new TextureRender();
+        }
+
+        public void setMediaPlayer(MediaPlayer player) {
+            mMediaPlayer = player;
+        }
+
+        public void onDrawFrame(GL10 glUnused) {
+            synchronized(this) {
+                if (updateSurface) {
+                    mSurfaceTexture.updateTexImage();
+                    updateSurface = false;
+                }
+            }
+
+            mTextureRender.drawFrame(mSurfaceTexture);
+        }
+
+        public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        }
+
+        public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+            mTextureRender.surfaceCreated();
+
+            /*
+             * Create the SurfaceTexture that will feed this textureID,
+             * and pass it to the MediaPlayer
+             */
+            mSurfaceTexture = new SurfaceTexture(mTextureRender.getTextureId());
+            mSurfaceTexture.setOnFrameAvailableListener(this);
+
+            Surface surface = new Surface(mSurfaceTexture);
+            mMediaPlayer.setSurface(surface);
+            surface.release();
+
+            try {
+                mMediaPlayer.prepare();
+            } catch (IOException t) {
+                Log.e(TAG, "media player prepare failed");
+            }
+
+            synchronized(this) {
+                updateSurface = false;
+            }
+            mMediaPlayer.start();
+        }
+
+        synchronized public void onFrameAvailable(SurfaceTexture surface) {
+            updateSurface = true;
+        }
+    }  // End of class VideoRender.
+
+}  // End of class VideoSurfaceView.
---
+++ RBPlayer/src/org/zhangge/rbplayer/ui/MediaPlayerActivity.java
@@ -0,0 +1,51 @@
+package org.zhangge.rbplayer.ui;
+
+import org.zhangge.rbplayer.lib.VideoSurfaceView;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MediaPlayerActivity extends Activity {
+
+	private MediaPlayer player;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		try {
+			player = new MediaPlayer();
+			player.reset();
+			player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+			player.setDataSource(Environment.getExternalStorageDirectory() + "/Tongli3D-II/videores/Ì«Æ½Ñó(00h32m49s-01h05m38s).mp4");
+			VideoSurfaceView vsv = new VideoSurfaceView(this, player);
+			setContentView(vsv);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		player.pause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		player.stop();
+	}
+	
+	
+}
