288795835a3f1051a99b5ca8ee48ce0e11dd3b8f
张戈 <zhgeaits@gmail.com>
2015-03-15 23:43:00 +0800

    colorview

--- RBPlayer/src/org/zhangge/rbplayer/camera/PicTextureRender.java
+++ RBPlayer/src/org/zhangge/rbplayer/camera/PicTextureRender.java
@@ -10,7 +10,6 @@ import javax.microedition.khronos.opengles.GL10;
 import org.zhangge.rbplayer.utils.ShaderUtils;
 
 import android.graphics.Bitmap;
-import android.opengl.GLES11Ext;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLUtils;
@@ -30,23 +29,45 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
 
 	private static final String VERTEX_SHADER = 
 			"uniform mat4 uMVPMatrix;\n" +
-            "uniform mat4 uSTMatrix;\n" +
             "attribute vec4 aPosition;\n" +
             "attribute vec4 aTextureCoord;\n" +
+            "attribute vec2 aTexCoor;\n" +
             "varying vec2 vTextureCoord;\n" +
             "void main() {\n" +
             "  gl_Position = uMVPMatrix * aPosition;\n" +
-            "  vTextureCoord = (uSTMatrix * aTextureCoord).xy;\n" +
+            "  vTextureCoord = aTexCoor;\n" +
             "}\n";
 
 	private static final String FRAGMENT_SHADER = 
 			"#extension GL_OES_EGL_image_external : require\n" +
-		    "precision highp float;\n" +
-		    "varying vec2 vTextureCoord;\n" +
-		    "uniform samplerExternalOES sTexture;\n" +
-		    "void main() {\n" +
-		    "    gl_FragColor = texture2D(sTexture, vTextureCoord);\n" +
-		    "}\n";
+            "precision highp float;\n" +
+            "varying vec2 vTextureCoord;\n" +
+            "uniform samplerExternalOES sTexture;\n" +
+            "uniform int mode;\n" +
+            "uniform float offset;\n" +
+            "uniform float width;\n" +
+            "void doModeOne(){\n" +
+            "  float origx = vTextureCoord.x;\n" +
+            "  if(origx < 0.5) {\n" +
+            "	vec2 vTexL;\n" +
+            "	vec2 vTexR;\n" +
+            "	vTexL.y = vTextureCoord.y;\n" +
+            "	vTexR.y = vTextureCoord.y;\n" +
+            "	vTexL.x = vTextureCoord.x;\n" +
+            "	vTexR.x = vTextureCoord.x + 0.5 + offset / width;\n" +
+            "	gl_FragColor.r = texture2D(sTexture, vTexL).r;\n" +
+            "  	gl_FragColor.gba = texture2D(sTexture, vTexR).gba;\n" +
+            "  } else {\n" +
+            "   discard;\n"+
+            "  }\n" +
+            "}\n" +
+            "void main() {\n" +
+            "   if(mode == 1) {\n" +
+            "       doModeOne();\n" +
+            "   } else {\n" +
+            "       gl_FragColor = texture2D(sTexture, vTextureCoord);\n" +
+            "   }\n" +
+            "}\n";
 
 	private float[] mMVPMatrix = new float[16];
 	private float[] mSTMatrix = new float[16];
@@ -58,12 +79,15 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
 	private int maTextureHandle;
 	private int mWidthHandle;
 	private int mOffsetHandle;
+	private int mModeHandle;
+	int maTexCoorHandle;
 
 	private int[] texturesIds;
 	private Bitmap gBitmap1;
 	private Bitmap gBitmap2;
 	private float gOffset = 0.0f;
 	private float mWidth;
+	FloatBuffer   mTexCoorBuffer;//顶点纹理坐标数据缓冲
 
 	public PicTextureRender(Bitmap gBitmap1, Bitmap gBitmap2) {
 		super();
@@ -72,10 +96,26 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
 		gOffset = 0;
 		texturesIds = new int[2];
 
-		mTriangleVertices = ByteBuffer.allocateDirect(mTriangleVerticesData.length * FLOAT_SIZE_BYTES).order(ByteOrder.nativeOrder()).asFloatBuffer();
+		mTriangleVertices = ByteBuffer
+				.allocateDirect(mTriangleVerticesData.length * FLOAT_SIZE_BYTES)
+				.order(ByteOrder.nativeOrder())
+				.asFloatBuffer();
 		mTriangleVertices.put(mTriangleVerticesData).position(0);
 
 		Matrix.setIdentityM(mSTMatrix, 0);
+		
+		float texCoor[]=new float[]//顶点颜色值数组，每个顶点4个色彩值RGBA
+		  	    {
+		  	      		0,0, 0,1, 1,1,
+		  	      		1,1, 1,0, 0,0        		
+		  	    };  
+		        //创建顶点纹理坐标数据缓冲
+		        ByteBuffer cbb = ByteBuffer.allocateDirect(texCoor.length*4);
+		        cbb.order(ByteOrder.nativeOrder());//设置字节顺序
+		        mTexCoorBuffer = cbb.asFloatBuffer();//转换为Float型缓冲
+		        mTexCoorBuffer.put(texCoor);//向缓冲区中放入顶点着色数据
+		        mTexCoorBuffer.position(0);
+		        
 	}
 
 	public void setOffset(int offset) {
@@ -86,7 +126,7 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
 		int[] textures = new int[1];
 		GLES20.glGenTextures(1, textures, 0);
 		int textureId = textures[0];
-		GLES20.glEnable(GLES20.GL_CULL_FACE);
+		GLES20.glDisable(GLES20.GL_CULL_FACE);
 		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
 		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
 		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
@@ -110,11 +150,11 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
             throw new RuntimeException("Could not get attrib location for aPosition");
         }
         
-        maTextureHandle = GLES20.glGetAttribLocation(mProgram, "aTextureCoord");
-        ShaderUtils.checkGlError("glGetAttribLocation aTextureCoord");
-        if (maTextureHandle == -1) {
-            throw new RuntimeException("Could not get attrib location for aTextureCoord");
-        }
+//        maTextureHandle = GLES20.glGetAttribLocation(mProgram, "aTextureCoord");
+//        ShaderUtils.checkGlError("glGetAttribLocation aTextureCoord");
+//        if (maTextureHandle == -1) {
+//            throw new RuntimeException("Could not get attrib location for aTextureCoord");
+//        }
 
         muMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
         ShaderUtils.checkGlError("glGetUniformLocation uMVPMatrix");
@@ -122,23 +162,32 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
             throw new RuntimeException("Could not get attrib location for uMVPMatrix");
         }
 
-        muSTMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uSTMatrix");
-        ShaderUtils.checkGlError("glGetUniformLocation uSTMatrix");
-        if (muSTMatrixHandle == -1) {
-            throw new RuntimeException("Could not get attrib location for uSTMatrix");
+//        muSTMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uSTMatrix");
+//        ShaderUtils.checkGlError("glGetUniformLocation uSTMatrix");
+//        if (muSTMatrixHandle == -1) {
+//            throw new RuntimeException("Could not get attrib location for uSTMatrix");
+//        }
+        
+        maTexCoorHandle= GLES20.glGetAttribLocation(mProgram, "aTexCoor");
+        
+        mModeHandle = GLES20.glGetUniformLocation(mProgram, "mode");
+        ShaderUtils.checkGlError("glGetUniformLocation mode");
+        if(mModeHandle == -1) {
+        	throw new RuntimeException("Could not get attrib location for mode");
         }
 
-//        mWidthHandle = GLES20.glGetUniformLocation(mProgram, "width");
-//        ShaderUtils.checkGlError("glGetUniformLocation width");
-//        if(mWidthHandle == -1) {
-//        	throw new RuntimeException("Could not get attrib location for width");
-//        }
-//        
-//        mOffsetHandle = GLES20.glGetUniformLocation(mProgram, "offset");
-//        ShaderUtils.checkGlError("glGetUniformLocation offset");
-//        if(mOffsetHandle == -1) {
-//            throw new RuntimeException("Could not get attrib location for offset");
-//        }
+        mOffsetHandle = GLES20.glGetUniformLocation(mProgram, "offset");
+        ShaderUtils.checkGlError("glGetUniformLocation offset");
+        if(mOffsetHandle == -1) {
+        	throw new RuntimeException("Could not get attrib location for offset");
+        }
+        
+        mWidthHandle = GLES20.glGetUniformLocation(mProgram, "width");
+        ShaderUtils.checkGlError("glGetUniformLocation width");
+        if(mWidthHandle == -1) {
+        	throw new RuntimeException("Could not get attrib location for width");
+        }
+        
         
         texturesIds[0] = initTexture(gBitmap1);
 //		texturesIds[1] = initTexture(gBitmap2);
@@ -175,20 +224,32 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
         GLES20.glEnableVertexAttribArray(maPositionHandle);
         ShaderUtils.checkGlError("glEnableVertexAttribArray maPositionHandle");
 
-        mTriangleVertices.position(TRIANGLE_VERTICES_DATA_UV_OFFSET);
-        GLES20.glVertexAttribPointer(maTextureHandle, 2, GLES20.GL_FLOAT, false,
-            TRIANGLE_VERTICES_DATA_STRIDE_BYTES, mTriangleVertices);
-        ShaderUtils.checkGlError("glVertexAttribPointer maTextureHandle");
-        
-        GLES20.glEnableVertexAttribArray(maTextureHandle);
-        ShaderUtils.checkGlError("glEnableVertexAttribArray maTextureHandle");
+//        mTriangleVertices.position(TRIANGLE_VERTICES_DATA_UV_OFFSET);
+//        GLES20.glVertexAttribPointer(maTextureHandle, 2, GLES20.GL_FLOAT, false,
+//            TRIANGLE_VERTICES_DATA_STRIDE_BYTES, mTriangleVertices);
+//        ShaderUtils.checkGlError("glVertexAttribPointer maTextureHandle");
+//        
+//        GLES20.glEnableVertexAttribArray(maTextureHandle);
+//        ShaderUtils.checkGlError("glEnableVertexAttribArray maTextureHandle");
 
         Matrix.setIdentityM(mMVPMatrix, 0);
         GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix, 0);
-        GLES20.glUniformMatrix4fv(muSTMatrixHandle, 1, false, mSTMatrix, 0);
+//        GLES20.glUniformMatrix4fv(muSTMatrixHandle, 1, false, mSTMatrix, 0);
+        GLES20.glVertexAttribPointer  
+        (
+       		maTexCoorHandle, 
+        		2, 
+        		GLES20.GL_FLOAT, 
+        		false,
+               2*4,   
+               mTexCoorBuffer
+        );   
 
-//        GLES20.glUniform1f(mOffsetHandle, gOffset);
-//        GLES20.glUniform1f(mWidthHandle, mWidth);
+        GLES20.glEnableVertexAttribArray(maTexCoorHandle);  
+        
+        GLES20.glUniform1f(mOffsetHandle, gOffset);
+        GLES20.glUniform1f(mWidthHandle, mWidth);
+        GLES20.glUniform1i(mModeHandle, 0);
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
         ShaderUtils.checkGlError("glDrawArrays");
--- RBPlayer/src/org/zhangge/rbplayer/ui/PhotoActivity.java
+++ RBPlayer/src/org/zhangge/rbplayer/ui/PhotoActivity.java
@@ -3,9 +3,8 @@ package org.zhangge.rbplayer.ui;
 import org.zhangge.almightyzgbox_android.net.http.VolleyManager;
 import org.zhangge.almightyzgbox_android.ui.bar.VerticalSeekBar;
 import org.zhangge.almightyzgbox_android.utils.CommonUtils;
-import org.zhangge.rbplayer.camera.MyGLRenderer;
+import org.zhangge.rbplayer.camera.ColorView;
 import org.zhangge.rbplayer.camera.PicTextureRender;
-import org.zhangge.rbplayer.camera.PicTextureRender2;
 import org.zhangge.rbplayerpro.R;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
@@ -36,7 +35,8 @@ public class PhotoActivity extends BaseActivity {
 	private Bitmap gBitmap1;
 	private Bitmap gBitmap2;
 	private int gOffset;
-	private MyGLRenderer gRender;
+	private PicTextureRender gRender;
+	private ColorView gColor;
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +44,8 @@ public class PhotoActivity extends BaseActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_photo);
         gImageView = (ImageView) findViewById(R.id.image);
-        gSurfaceView = (GLSurfaceView) findViewById(R.id.surfaceview);
+//        gSurfaceView = (GLSurfaceView) findViewById(R.id.surfaceview);
+        gColor = (ColorView) findViewById(R.id.colorimage);
         gOffsetSeekbar = (VerticalSeekBar) findViewById(R.id.offset_seekbar);
 		gOffsetSeekbar.setMax(OFFSET_MAX * 2);
 		gOffsetSeekbar.setProgress(OFFSET_MAX);
@@ -72,17 +73,19 @@ public class PhotoActivity extends BaseActivity {
 	private void initRBImage() {
 		gImageView.setVisibility(View.GONE);
 		gOffsetSeekbar.setVisibility(View.VISIBLE);
-		gSurfaceView.setVisibility(View.VISIBLE);
+//		gSurfaceView.setVisibility(View.VISIBLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); 
 		gPath2 = gPath1.replace(".jpg", "-2.jpg");
 		gBitmap1 = BitmapFactory.decodeFile(gPath1);
 		gBitmap2 = BitmapFactory.decodeFile(gPath2);
+		gColor.setBitmap(gBitmap1);
 		
-//		gRender = new PicTextureRender2(gBitmap1, gBitmap2);
-		gRender = new MyGLRenderer(this, gBitmap1);
-//		gSurfaceView.setEGLContextClientVersion(1);
-		gSurfaceView.setRenderer(gRender);
+//		gRender = new PicTextureRender(gBitmap1, gBitmap2);
+////		gRender = new MyGLRenderer(this, gBitmap1);
+//		gSurfaceView.setEGLContextClientVersion(2);
+//		gSurfaceView.setRenderer(gRender);
+//		gSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
 		
 //		Bitmap dst = ImageHandler.leftRightCombineTwoPicture(gBitmap1, gBitmap2, gOffset);
 //		gImageView.setImageBitmap(dst);
