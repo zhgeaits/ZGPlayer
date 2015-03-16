92d2ea637db55d54d7d126f18041045f9ece4ac6
张戈 <zhgeaits@gmail.com>
2015-03-16 17:00:39 +0800

    opengles

--- RBPlayer/src/org/zhangge/rbplayer/camera/PicTextureRender.java
+++ RBPlayer/src/org/zhangge/rbplayer/camera/PicTextureRender.java
@@ -19,45 +19,17 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
 	private Bitmap gBitmap;
 	private float gOffset = 0.0f;
 	private float gWidth;
-	
-	
-	private static final String VERTEX_SHADER =
-            "uniform mat4 uMVPMatrix;\n" +
-            "attribute vec4 a_position;\n" +
-            "attribute vec2 a_texCoord;\n" +
-            "varying vec2 v_texCoord;\n" +
-            "void main() {\n" +
-            "  gl_Position = uMVPMatrix * a_position;\n" +
-            "  v_texCoord = a_texCoord;\n" +
-            "}\n";
-
-    private static final String FRAGMENT_SHADER =
-            "precision highp float;\n" +
-            "varying vec2 v_texCoord;\n" +
-            "uniform sampler2D u_samplerTexture;\n" +
-            "void main() {\n" +
-            "   gl_FragColor = texture2D(u_samplerTexture, v_texCoord);\n" +
-            "}\n";
-	
-	public PicTextureRender(Bitmap bitmap) {
-		super();
-		this.gBitmap = bitmap;
-		gOffset = 0;
-	}
-
-	public void setOffset(int offset) {
-		gOffset = offset;
-	}
-	
-	private float[] mMVPMatrix = new float[16];
-	private int textureId;
-	private int program;
-	private int muMVPMatrixHandle;
-	private int attribPosition;
-	private int attribTexCoord;
-	private int uniformTexture;
-	private FloatBuffer vertex;
-	private ShortBuffer index;
+	private float[] gMVPMatrix = new float[16];
+	private int gTextureId;
+	private int gProgram;
+	private int gMVPMatrixHandle;
+	private int gWidthHandle;
+    private int gOffsetHandle;
+	private int gAttribPosition;
+	private int gAttribTexCoord;
+	private int gUniformTexture;
+	private FloatBuffer gVertex;
+	private ShortBuffer gIndex;
 	private float[] quadVertex = new float[] { 
 			-1f, 1f, 0.0f, // Position 0
 			0, 1.0f, // TexCoord 0
@@ -76,6 +48,51 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
 			(short) (3), // Position 3
 			(short) (0), // Position 0
 	};
+	
+	private static final String VERTEX_SHADER =
+            "uniform mat4 uMVPMatrix;\n" +
+            "attribute vec4 a_position;\n" +
+            "attribute vec2 a_texCoord;\n" +
+            "varying vec2 v_texCoord;\n" +
+            "void main() {\n" +
+            "  gl_Position = uMVPMatrix * a_position;\n" +
+            "  v_texCoord = a_texCoord;\n" +
+            "}\n";
+
+    private static final String FRAGMENT_SHADER =
+            "precision highp float;\n" +
+            "varying vec2 v_texCoord;\n" +
+            "uniform sampler2D u_samplerTexture;\n" +
+            "uniform float offset;\n" +
+            "uniform float width;\n" +
+            "void doModeOne(){\n" +
+            "  float origx = v_texCoord.x;\n" +
+            "  if(origx < 0.5) {\n" +
+            "	vec2 vTexL;\n" +
+            "	vec2 vTexR;\n" +
+            "	vTexL.y = v_texCoord.y;\n" +
+            "	vTexR.y = v_texCoord.y;\n" +
+            "	vTexL.x = v_texCoord.x;\n" +
+            "	vTexR.x = v_texCoord.x + 0.5 + offset / width;\n" +
+            "	gl_FragColor.r = texture2D(u_samplerTexture, vTexL).r;\n" +
+            "  	gl_FragColor.gba = texture2D(u_samplerTexture, vTexR).gba;\n" +
+            "  } else {\n" +
+            "   discard;\n"+
+            "  }\n" +
+            "}\n" +
+            "void main() {\n" +
+            "   doModeOne();\n" +
+            "}\n";
+	
+	public PicTextureRender(Bitmap bitmap) {
+		super();
+		this.gBitmap = bitmap;
+		gOffset = 0;
+	}
+
+	public void setOffset(int offset) {
+		gOffset = offset;
+	}
 	
 	public static int loadProgram(String vertexSource, String fragmentSource) {
 		int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource);
@@ -116,36 +133,32 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
 	
 	private void loadVertex() {
 		// float size = 4
-		this.vertex = ByteBuffer.allocateDirect(quadVertex.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
-		this.vertex.put(quadVertex).position(0);
+		this.gVertex = ByteBuffer.allocateDirect(quadVertex.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
+		this.gVertex.put(quadVertex).position(0);
 		// short size = 2
-		this.index = ByteBuffer.allocateDirect(quadIndex.length * 2).order(ByteOrder.nativeOrder()).asShortBuffer();
-		this.index.put(quadIndex).position(0);
+		this.gIndex = ByteBuffer.allocateDirect(quadIndex.length * 2).order(ByteOrder.nativeOrder()).asShortBuffer();
+		this.gIndex.put(quadIndex).position(0);
 	}
 	
 	private void initShader() {
 		String vertexSource = VERTEX_SHADER;
 		String fragmentSource = FRAGMENT_SHADER;
 		// Load the shaders and get a linked program
-		program = loadProgram(vertexSource, fragmentSource);
+		gProgram = loadProgram(vertexSource, fragmentSource);
 		
 		// Get the attribute locations
-		muMVPMatrixHandle = GLES20.glGetUniformLocation(program, "uMVPMatrix");
-//        ShaderUtils.checkGlError("glGetUniformLocation uMVPMatrix");
-//        if (muMVPMatrixHandle == -1) {
-//            throw new RuntimeException("Could not get attrib location for uMVPMatrix");
-//        }
-		
-		attribPosition = GLES20.glGetAttribLocation(program, "a_position");
-		attribTexCoord = GLES20.glGetAttribLocation(program, "a_texCoord");
-		uniformTexture = GLES20.glGetUniformLocation(program, "u_samplerTexture");
-		GLES20.glUseProgram(program);
-		GLES20.glEnableVertexAttribArray(attribPosition);
-		GLES20.glEnableVertexAttribArray(attribTexCoord);
-		
+		gMVPMatrixHandle = GLES20.glGetUniformLocation(gProgram, "uMVPMatrix");
+		gAttribPosition = GLES20.glGetAttribLocation(gProgram, "a_position");
+		gAttribTexCoord = GLES20.glGetAttribLocation(gProgram, "a_texCoord");
+		gUniformTexture = GLES20.glGetUniformLocation(gProgram, "u_samplerTexture");
+		gWidthHandle = GLES20.glGetUniformLocation(gProgram, "width");
+        gOffsetHandle = GLES20.glGetUniformLocation(gProgram, "offset");
+		GLES20.glUseProgram(gProgram);
+		GLES20.glEnableVertexAttribArray(gAttribPosition);
+		GLES20.glEnableVertexAttribArray(gAttribTexCoord);
 		
 		// Set the sampler to texture unit 0
-		GLES20.glUniform1i(uniformTexture, 0);
+		GLES20.glUniform1i(gUniformTexture, 0);
 	}
 	
 	private int loadTexture(Bitmap bitmap) {
@@ -179,7 +192,7 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
 		GLES20.glActiveTexture(GLES20.GL_TEXTURE0); 
 		loadVertex(); 
 		initShader(); 
-		textureId = loadTexture(gBitmap);
+		gTextureId = loadTexture(gBitmap);
 	}
 
 	@Override
@@ -190,26 +203,29 @@ public class PicTextureRender implements GLSurfaceView.Renderer {
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
-		// clear screen to black
 		GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
+		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, gTextureId);
 		
-		vertex.position(0);
+		gVertex.position(0);
 		// load the position
 		// 3(x , y , z)
 		// (2 + 3 )* 4 (float size) = 20
-		GLES20.glVertexAttribPointer(attribPosition, 3, GLES20.GL_FLOAT, false, 20, vertex);
-		vertex.position(3);
+		GLES20.glVertexAttribPointer(gAttribPosition, 3, GLES20.GL_FLOAT, false, 20, gVertex);
+		gVertex.position(3);
 		// load the texture coordinate
-		GLES20.glVertexAttribPointer(attribTexCoord, 2, GLES20.GL_FLOAT, false, 20, vertex);
+		GLES20.glVertexAttribPointer(gAttribTexCoord, 2, GLES20.GL_FLOAT, false, 20, gVertex);
 		
-		Matrix.setIdentityM(mMVPMatrix, 0);
-		mMVPMatrix[5] = -1;
-		Matrix.scaleM(mMVPMatrix, 0, 2f, 1f, 1f);
-    	Matrix.translateM(mMVPMatrix, 0, 0.5f, 0, 0);
-		GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix, 0);
+		Matrix.setIdentityM(gMVPMatrix, 0);
+		//不明白为什么会翻转了，这里就设置关于X轴对称
+		gMVPMatrix[5] = -1;
+		Matrix.scaleM(gMVPMatrix, 0, 2f, 1f, 1f);
+    	Matrix.translateM(gMVPMatrix, 0, 0.5f, 0, 0);
+		GLES20.glUniformMatrix4fv(gMVPMatrixHandle, 1, false, gMVPMatrix, 0);
 		
-		GLES20.glDrawElements(GLES20.GL_TRIANGLES, 6, GLES20.GL_UNSIGNED_SHORT, index);
+		GLES20.glUniform1f(gOffsetHandle, gOffset);
+        GLES20.glUniform1f(gWidthHandle, gWidth);
+		
+		GLES20.glDrawElements(GLES20.GL_TRIANGLES, 6, GLES20.GL_UNSIGNED_SHORT, gIndex);
 	}
 }
